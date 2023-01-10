9342fab925afa3f72dc4eb0be0d59d4aa16f3e5c
omar saadeh <omarsaadeh11@gmail.com>
2023-01-10 13:42:49 -0800

    Authen and Auth

--- startup/test.js
+++ startup/test.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db =
-  "mongodb+srv://omar:omaromar@cluster0.l9nezt7.mongodb.net/tt?retryWrites=true&w=majority";
-mongoose.connect(db, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successful");
-  }
-});
+// const mongoose = require("mongoose");
+// const db =
+//   "mongodb+srv://omar:omaromar@cluster0.l9nezt7.mongodb.net/tt?retryWrites=true&w=majority";
+// mongoose.connect(db, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
